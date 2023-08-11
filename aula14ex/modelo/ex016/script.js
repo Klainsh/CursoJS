efbba65b8a87f395847d38128b0e950c0548196e
Klainsh <60084904+Klainsh@users.noreply.github.com>
2023-08-10 21:53:05 -0300

    exercicio tabuada

--- aula14ex/modelo/ex016/script.js
+++ aula14ex/modelo/ex016/script.js
@@ -3,21 +3,22 @@ function contar(){
     var fim = document.getElementById('fim')
     var passos = document.getElementById('passos')
     var resultado = document.getElementById('resultado')
-    var inicio_tratado = Number(inicio.value)
-    var fim_tratado = Number(fim.value)
-    var passos_tratado = Number(passos.value)
+
     if(inicio.value.length == 0){
         alert('Digite o valor inicial.')
     }else if(fim.value.length == 0){
         alert('Digite o valor final.')
     }else if(passos.value.length == 0){
         alert('Digite os passos.')
-    }else{    
-        resultado.innerHTML = ''
+    }else{      
+        let inicio_tratado = Number(inicio.value)
+        let fim_tratado = Number(fim.value)
+        let passos_tratado = Number(passos.value)
+        resultado.innerHTML = 'Contando: </br>'
+        
         for(inicio_tratado; inicio_tratado <= fim_tratado; inicio_tratado += passos_tratado){
-            resultado.innerHTML += `${inicio_tratado}`
+            resultado.innerHTML += `${inicio_tratado},`
         }
     }
-
     
 }
